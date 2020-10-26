415803896616de7f8014a681dd9324e21f59dee0
Tatyana <tatyanabortnik@gmail.com>
2020-10-26 22:15:08 +0200

    update

---
+++ markup/js/slick-settings.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+                $('.slick-slider').slick({
+                	centerMode: true,
+  					centerPadding: '20px',
+					
+					 
+					  slidesToShow: 3,
+					  // slidesToScroll: 3,
+					  arrows: true,
+
+					  
+					});
+                });
